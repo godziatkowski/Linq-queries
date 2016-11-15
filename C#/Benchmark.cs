73c9885ff8de561298cba1a8d08852b47138815e
Godzio <shaureith@gmail.com>
2016-11-15 19:12:28 +0100

    Linq queries *Custom - queries with results *Benchmark - execution times for queries from Custom *Parallel - parallel execution times for queries from Custom *Times.txt contains meassured (average) times and queries

---
+++ C#/Benchmark.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace QuerySamples
+{
+    class Benchmark
+    {
+        private static readonly int LOOP_STOPPER = 5;
+
+        public static double getAverageTime(Func<IEnumerable<object>> func)
+        {
+            double result = 0;
+            for (int i = 0; i < LOOP_STOPPER; i++)
+            {
+                Stopwatch watch = Stopwatch.StartNew();
+
+                func
+                    .Invoke()
+                    .Count();
+
+                watch.Stop();
+
+                result += watch.Elapsed.TotalMilliseconds;
+            }
+            return result / LOOP_STOPPER;
+        }
+
+        public static double getTimeOf(Func<IEnumerable<object>> func)
+        {
+            double result = 0;
+            for (int i = 0; i < LOOP_STOPPER; i++)
+            {
+                Stopwatch watch = Stopwatch.StartNew();
+
+                func
+                    .Invoke()
+                    .Count();
+
+                watch.Stop();
+
+                result += watch.Elapsed.TotalMilliseconds;
+            }
+            return result / LOOP_STOPPER;
+        }
+    }
+}
---
+++ C#/Product.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQTest
+{
+    public class Product
+    {
+        public Product() { }
+
+        public Product(int productID, String productName, String category,
+                double unitPrice, int unitsInStock)
+        {
+            this.ProductID = productID;
+            this.ProductName = productName;
+            this.Category = category;
+            this.UnitPrice = unitPrice;
+            this.UnitsInStock = unitsInStock;
+        }
+
+        public int ProductID;
+        public String ProductName;
+        public String Category;
+        public double UnitPrice;
+        public int UnitsInStock;
+
+        public override String ToString()
+        {
+            return "Product[productID=" + ProductID + ", ProductName=" + ProductName + ", Category=" + Category + ", UnitPrice=" + UnitPrice + ", UnitsInStock=" + UnitsInStock + "]";
+        }
+
+        public override int GetHashCode()
+        {
+            const int prime = 31;
+            int result = 1;
+            result = prime * result
+                    + ((Category == null) ? 0 : Category.GetHashCode());
+            result = prime * result + ProductID;
+            result = prime * result
+                    + ((ProductName == null) ? 0 : ProductName.GetHashCode());
+            long temp;
+            temp = BitConverter.DoubleToInt64Bits(UnitPrice);
+            result = prime * result + (int)(temp ^ (int)((uint)temp >> 32));
+            result = prime * result + UnitsInStock;
+            return result;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (GetType() != obj.GetType())
+                return false;
+            Product other = (Product)obj;
+            if (Category == null)
+            {
+                if (other.Category != null)
+                    return false;
+            }
+            else if (!Category.Equals(other.Category))
+                return false;
+            if (ProductID != other.ProductID)
+                return false;
+            if (ProductName == null)
+            {
+                if (other.ProductName != null)
+                    return false;
+            }
+            else if (!ProductName.Equals(other.ProductName))
+                return false;
+            if (BitConverter.DoubleToInt64Bits(UnitPrice) != BitConverter.DoubleToInt64Bits(other.UnitPrice))
+                return false;
+            if (UnitsInStock != other.UnitsInStock)
+                return false;
+            return true;
+        }
+    }
+}
